7c70d0b8261a95aa9740873a182131ae85506fad
kezzme <la.torre.troy09@gmail.com>
2023-02-01 20:14:30 +0800

    Initial Commit

--- Products.php
+++ Products.php
@@ -1,33 +1,32 @@
 <?php
-class Products () {
-private $name;
-private $description;
-private $price;
-private $srp;
+    class Products{
+        private $name;
+        private $description;
+        private $price;
+        private $srp;
+    
+        function setName($name){
+            $this->name = $name;
+        }
+        function setDescription($description){
+            $this->description = $description;
+        }
+        function setPrice($price){
+            $this->price = $price;
+        }
+        
+        function getName(){
+            return $this->name;
+        }
+        function getDescription(){
+            return $this->description;
+        }
+        function getPrice(){
+            return $this->price;
+        }
+        function computeSRP() {
+            return $this->price * 1.5;
+        }
+    }
 
-
-function setName() {
-    $this ->name =$name;
-}
-function setDescription() {
-    $this ->description =$description;
-}
-function setPrice() {
-    $this ->price =$price;
-}
-
-function getName() {
-    echo $this ->name;
-}
-function getDescription() {
-    echo $this ->description;
-}
-function getPrice() {
-    echo $this ->price;
-}
-
-function computeSRP() {
-    echo ($this-> price * 1.5;)
-}
-
-}
+?>
